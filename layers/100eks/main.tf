406e8cf184663e580f5a88a2e3b77825a221ec18
Antonio Cuenco <antonio.cuenco@rackspace.com>
2020-12-12 14:09:19 +1100

    eks cluster

--- layers/100eks/main.tf
+++ layers/100eks/main.tf
@@ -10,20 +10,20 @@ provider "aws" {
   allowed_account_ids = [var.aws_account_id]
 }
 
-# provider "kubernetes" {
-#   host                   = data.aws_eks_cluster.cluster.endpoint
-#   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-#   token                  = data.aws_eks_cluster_auth.cluster.token
-#   load_config_file       = false
-# }
-#
-# data "aws_eks_cluster" "cluster" {
-#   name = module.eks.cluster_id
-# }
-#
-# data "aws_eks_cluster_auth" "cluster" {
-#   name = module.eks.cluster_id
-# }
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  load_config_file       = false
+}
+
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
+}
 
 locals {
   tags = {
@@ -130,3 +130,30 @@ resource "aws_security_group" "all_worker_mgmt" {
 ###############################################################################
 # EKS
 ###############################################################################
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  cluster_name    = var.cluster_name
+  cluster_version = "1.18"
+  subnets         = local.private_subnets
+  version         = "13.2.1"
+
+  cluster_create_timeout          = "1h"
+  cluster_endpoint_private_access = true
+
+  vpc_id = local.vpc_id
+
+  worker_groups = [
+    {
+      name                          = "worker-group-1"
+      instance_type                 = "t2.small"
+      additional_userdata           = "echo foo bar"
+      asg_desired_capacity          = 1
+      additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
+    },
+  ]
+
+  worker_additional_security_group_ids = [aws_security_group.all_worker_mgmt.id]
+  # map_roles                            = var.map_roles
+  # map_users                            = var.map_users
+  # map_accounts                         = var.map_accounts
+}
